c175fe1992a6a4968e59448b7a4f687c19f90537
Lilluther <168745703+Lilluther@users.noreply.github.com>
2025-01-04 23:19:32 +0300

    Create xProblem1(for loop).c

---
+++ xProblem1(for loop).c
@@ -0,0 +1,13 @@
+// online problem I found on X
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int i, sum=0;
+    for(i=0; i<5; i++)
+        sum +=i;
+    
+    printf("Sum is: %d ",sum);
+
+    return 0;
+}
